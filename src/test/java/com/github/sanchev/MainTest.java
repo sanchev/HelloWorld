0781abdfc1c97a646913502482ac99bc8a2ccdfb
sanchev <sanchev_temp@inbox.ru>
2021-11-23 21:13:41 +0200

    0.1.0
    Added Test

---
+++ src/test/java/com/github/sanchev/MainTest.java
@@ -0,0 +1,12 @@
+package com.github.sanchev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest  {
+    @Test
+    public void test1() {
+        assertTrue(false);
+    }
+}
